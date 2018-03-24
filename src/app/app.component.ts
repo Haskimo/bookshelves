aafc2f1cf07a1755f8d6c5f3d06a26fbd92c6b74
Haskimo <33072956+Haskimo@users.noreply.github.com>
2018-03-24 17:00:07 +0100

    AAAAA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  contructor() {
+    var config = {
+    apiKey: "AIzaSyAuDdMqOi9CPW4L96YLtdrSd9WyZV_AMl4",
+    authDomain: "http-client-demo-77161.firebaseapp.com",
+    databaseURL: "https://http-client-demo-77161.firebaseio.com",
+    projectId: "http-client-demo-77161",
+    storageBucket: "http-client-demo-77161.appspot.com",
+    messagingSenderId: "530045402853"
+  };
+  firebase.initializeApp(config);
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+import * as firebase from 'firebase';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  // Création nouvel utilisateur
+  createNewUser(email: string, password: string) {
+    return new Promise( // Méthode asynchrone
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then( //.then parce qu'il y a une Promise
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Connexion utilisateur déjà existant
+  signInUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  // Déconnexion utilisateur
+  signOutUser() {
+    firebase.auth().signOut();
+  }
+
+}
