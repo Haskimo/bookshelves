b7e0f6839e5ef8437749041540f1412dceb0a139
Haskimo <33072956+Haskimo@users.noreply.github.com>
2018-03-25 16:27:52 +0200

    Livres dans Firebase

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private booksService: BooksService, private router: Router) {
+
   }
 
+  ngOnInit() {
+    this.book = new Book('', ''); // D'abord un book vide car la méthode pour récupérer un livre est asynchrone et peut prendre du temps, sinon la page plante
+    const id = this.route.snapshot.params['id'];
+    this.booksService.getSingleBook(+id).then(
+      (book: Book) => {
+        this.book = book;
+      }
+    );
+  }
+
+  onBack() { // Pour le retour en arrière
+    this.router.navigate(['/books']);
+  }
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,8 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/book.model';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>(); // Qui emmetra l'array books
 
+  emitBooks() {
+    this.booksSubject.next(this.books);
+  }
+
+  saveBooks() {
+    firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks() {
+    firebase.database().ref('/books') // Méthode on va réagir à chaque fois qu'il trouve un livre dans la liste (temps réel en cas multi-utilisateurs)
+      .on('value', (data: DataSnapshot) => { // Sur l'évènement "value" qui retourne un objet DataSnapshot
+          this.books = data.val() ? data.val() : []; // On reçoit quelque chose ? ou pas []
+          this.emitBooks();
+        }
+      );
+  }
+
+  getSingleBook(id: number) { // Asynchrone car pas besoin de voir en temps réel
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks(); // sur le serveur
+    this.emitBooks(); // emettre le subject
+  }
+
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex( // Trouve l'index de ce livre dans l'array
+      (bookEl) => {
+        if(bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1); // Splice modifie l'array
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
